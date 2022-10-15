ccb59d0c6286b4ab6453d309b52607e441b13109
maix00 <59402841+maix00@users.noreply.github.com>
2022-10-15 17:41:46 +0800

    Update Syllabus Links

--- assets/scripts/syllabus2web.py
+++ assets/scripts/syllabus2web.py
@@ -30,5 +30,20 @@ for idx in range(len(namelist)):
     else:
         with open(to_target_folder + namelist[idx][0] + '.md', "w") as file:
             file.write(
-                '---\ntitle: \'[' + namelist[idx][0] +']\'\nlayout: single\npermalink: /courses/class-id/' + temp + '-' + temp2 + '\nauthor_profile: true\ntoc: true\ntoc_label: \'目录\'\ntoc_sticky: true\n---\n\n<div class=\'notice--warning\'>\n<p><i><a rel=\'license\' href=\'http://creativecommons.org/licenses/by-nc-sa/4.0/\'><img alt=\'Creative Commons License\' style=\'border-width:0\' src=\'https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png\' /></a><br /> The content of this webpage is licensed under a <a rel=\'license\' href=\'http://creativecommons.org/licenses/by-nc-sa/4.0/\'>Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</i></p>\n</div>\n\n<a href=\'https://fdu-math.github.io/courses/' + temp +'\'>链接至[' + temp + ']课程页面<a>\n\n# ' + namelist[idx][1] + '\n\n<a href=\'https://fdu-math.github.io/assets/docs/courses/' + namelist0[idx][0] + '-' + namelist0[idx][1] + '-' + namelist0[idx][2] + '-' + namelist0[idx][3] + ' (Encrypted).pdf\'>课程大纲</a>'
+                '---\n'
+                'title: \'[' + namelist[idx][0] +']\'\n'
+                'layout: single\n'
+                'permalink: /courses/class-id/' + temp + '-' + temp2 + '\n'
+                'author_profile: true\ntoc: true\n'
+                'toc_label: \'目录\'\n'
+                'toc_sticky: true\n'
+                '---\n'
+                '\n'
+                '<div class=\'notice--warning\'>\n'
+                '\t<p><i><a rel=\'license\' href=\'http://creativecommons.org/licenses/by-nc-sa/4.0/\'><img alt=\'Creative Commons License\' style=\'border-width:0\' src=\'https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png\' /></a><br /> The content of this webpage is licensed under a <a rel=\'license\' href=\'http://creativecommons.org/licenses/by-nc-sa/4.0/\'>Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</i></p>\n'
+                '</div>\n'
+                '\n'
+                '<a href=\'https://fdu-math.github.io/courses/' + temp +'\'>链接至[' + temp + ']课程页面<a>\n\n# ' + namelist[idx][1] + '\n'
+                '\n'
+                '<a href=\'../courses/syllabus/' + namelist0[idx][0] + '-' + namelist0[idx][1] + '-' + namelist0[idx][2] + '-' + namelist0[idx][3] + ' (Encrypted).pdf\'>课程大纲</a>'
             )
