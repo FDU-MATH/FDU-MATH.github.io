52f0e4a798c95b6619bed6d44000aab0bd3f39e8
maix00 <59402841+maix00@users.noreply.github.com>
2022-10-16 19:34:14 +0800

    Update Courses Page Automation
    Wrongly set href for courses on this page

--- assets/scripts/course2web.py
+++ assets/scripts/course2web.py
@@ -13,7 +13,7 @@ def courses2html(file, target_html):
         tmp = file['教师'].values.tolist()[idx]
         tmp2 = file['职称'].values.tolist()[idx]
         tmp3 = file['课程序号'].values.tolist()[idx]
-        tmp4 = file["课程名称"].values.tolist()[idx]
+        # tmp4 = file["课程名称"].values.tolist()[idx]
         tmp5 = copy.deepcopy(tmp3)
         tmp = tmp.split(',')
         tmp2 = tmp2.split(',')
@@ -24,7 +24,7 @@ def courses2html(file, target_html):
                 tmp2[idxx] = 'Null'
         file.iloc[idx, 3] = '<br />'.join(tmp)
         file.iloc[idx, 4] = '<br />'.join(tmp2)
-        file.iloc[idx, 1] = '<a href=\'https://fdu-math.github.io/courses/' + tmp3[0] + '\'>' + tmp4 + '</a>'
+        # file.iloc[idx, 1] = '<a href=\'https://fdu-math.github.io/courses/' + tmp3[0] + '\'>' + tmp4 + '</a>'
         file.iloc[idx, 0] = '<a href=\'https://fdu-math.github.io/courses/class-id/' + tmp3[0] + '-' + tmp3[1] + '\'>' + tmp5 + '</a>'
     file.to_html(test_html, index=False)
     temp1 = []
